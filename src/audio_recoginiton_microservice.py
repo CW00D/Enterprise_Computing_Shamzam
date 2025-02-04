be0141e7b0b35444ee0f91ee9fcfcec6ac8d8c9d
Christian Wood <christian.f.wood@gmail.com>
2025-02-04 20:49:48 +0000

    Added basic request functionality to audio recognition microservice. Note the implementation of the call to AUDD.io api is not implemented yet

--- src/audio_recoginiton_microservice.py
+++ src/audio_recoginiton_microservice.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 
 with open("config/credentials.json") as infile:
@@ -7,12 +7,15 @@ with open("config/credentials.json") as infile:
     KEY = json_obj.get("AUDDIO_TOKEN", "")
 
 app = Flask(__name__)
+CATALOGUE_URL = "http://localhost:3000"
 
 @app.route("/recognise",methods=["POST"])
 def recognise():
-    pass
-    #HANDLE 200 (OK)
-    #HANDLE 400 (BAD REQUEST)
+    #Handle all the auddio data requesting etc
+    title = "Placeholder Title"
+    artist = "Placeholder Artist"
+    response = requests.get(f"{CATALOGUE_URL}/tracks/search?title={title}&artist={artist}")
+    return jsonify(response.json()), response.status_code
 
 if __name__ == "__main__":
     app.run(host="localhost",port=3002)
