17442e70ae86a5e9fe3848cd6c1df04f7aa7e990
Christian Wood <christian.f.wood@gmail.com>
2025-02-27 18:44:02 +0000

    Moved database helper into database management microservice directory

--- src/Database_management_microservice/database_management_microservice.py
+++ src/Database_management_microservice/database_management_microservice.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 import logging
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from database_helper import MusicTrackDatabase
 
 # Define log directory within the microservice folder
